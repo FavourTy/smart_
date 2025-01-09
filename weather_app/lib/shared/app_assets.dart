37db6722485f661b0a606e1e4fe07aa4681a2ca3
favourTy <favouradetayo03@gmail.com>
2025-01-09 10:01:54 +0100

    Added sca-weather-app as a submodule

---
+++ weather_app/lib/shared/app_assets.dart
@@ -0,0 +1,7 @@
+class AppAssets {
+  static const String base = 'assets/images';
+  static const String drizzlingPng = '${base}drizzling.png';
+  static const String snowyPng = '${base}snow.png';
+  static const String stormyPng = '${base}stormy.png';
+  static const String sunnyPng = '${base}sunny.png';
+}

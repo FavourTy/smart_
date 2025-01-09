82e428a6c766164c87738df143c32863dcf34cc8
favourTy <favouradetayo03@gmail.com>
2025-01-09 18:31:45 +0100

    set up

---
+++ weather_app/lib/shared/navigation/app_router.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:weather_app/features/screens/home_screen/views/home_screen.dart';
+import 'package:weather_app/features/screens/weather_details_screen/weather_details_screen.dart';
+
+import 'app_route_strings.dart';
+
+class AppRouter {
+  static final navKey = GlobalKey<NavigatorState>();
+
+  static Route appRouter(RouteSettings settings) {
+    switch (settings.name) {
+      case AppRouteStrings.base:
+        return CupertinoPageRoute(builder: (_) => const HomeScreen());
+      case AppRouteStrings.weatherDetailScreen:
+        return CupertinoPageRoute(builder: (_) => WeatherDetailsScreen());
+      default:
+        return CupertinoPageRoute(builder: (_) => const SizedBox());
+    }
+  }
+
+  static void pushNamed({required String routeName, Object? arg}) {
+    navKey.currentState?.pushNamed(routeName, arguments: arg);
+  }
+}

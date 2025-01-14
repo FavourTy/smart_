01803fe014fb75eedfee616a0b6a3bf011da431f
favourTy <favouradetayo03@gmail.com>
2025-01-14 01:51:34 +0100

    dotenv setup

--- weather_app/lib/main.dart
+++ weather_app/lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:weather_app/shared/navigation/app_route_strings.dart';
 import 'package:weather_app/shared/navigation/app_router.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 

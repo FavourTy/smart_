644db3d717876a4c72aa764c591a6f795d608a40
favourTy <favouradetayo03@gmail.com>
2025-01-25 23:21:07 +0100

    🚧flowww setup done

---
+++ floww/lib/shared/app_assets.dart
@@ -0,0 +1,12 @@
+class AppAssets {
+  static const String _base = "assets/images/";
+  static const String activeMsg = "${_base}active_msg.png";
+  static const String attachment = "${_base}attachment.png";
+  static const String back = "${_base}back.png";
+  static const String cancel = "${_base}cancel.png";
+  static const String customer = "${_base}customer.png";
+  static const String files = "${_base}files.png";
+  static const String message = "${_base}message.png";
+  static const String search = "${_base}search.png";
+  static const String settings = "${_base}settings.png";
+}
---
+++ floww/lib/shared/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF5DB075);
+  static const Color textFormFieldColor = Color(0xFFBDBDBD);
+  static const Color backgroundColor = Colors.white;
+  static const Color tabBarColor = Color(0xFFEEFAF8);
+  static const Color secColor = Color(0xFFEEFAF8);
+  static const Color newsLetterColor = Color(0xFF666666);
+  static const Color textCardcolor = Color(0xFFF2F7FB);
+}
